c36444eaad5594a7f11cd0140ac6ef32896d495b
ferandosanchez1128 <fernandosanchez1128@gmail.com>
2016-11-03 21:52:47 -0500

    adiccion de captura de excepcion y rango de años mas grande

--- hilos_descarga.py
+++ hilos_descarga.py
@@ -9,12 +9,15 @@ import os
 def worker(query,tema,year):
     administrador = AdministradorConsultas()
     print query
-    administrador.consulta(query,tema,year)
+    try:
+        administrador.consulta(query,tema,year)
+    except:
+        print "descarga finalizada"
 
 temas = ['astronomy', 'medicine', 'politics', 'robotics']
 directory = "/home/master/doc_pruebas2/"
 for tema in temas:
-    for year in range (2012,2017):
+    for year in range (2010,2017):
 
         query = 'TITLE-ABS-KEY (' + tema + ')  AND  DOCTYPE (ar OR re ) AND  LANGUAGE ( english )  AND  PUBYEAR  = '+ str (year)
         ruta  = directory + tema + '.' + str (year)
